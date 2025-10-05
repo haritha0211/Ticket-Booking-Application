34bdb4880c999d9e2af4cbe5bcef3180bdee8902
haritha0211 <136001235+haritha0211@users.noreply.github.com>
2025-10-05 09:48:31 +0530

    Delete backend directory

--- backend/src/main/java/com/ticketbooking/service/ISeatService.java
+++
@@ -1,9 +0,0 @@
-package com.ticketbooking.service;
-
-import com.ticketbooking.model.Seat;
-
-public interface ISeatService {
-    Seat bookSeat(Seat seat);
-    Seat cancelSeatBooking(Seat seat);
-    Seat blockSeat(Seat seat);
-}
